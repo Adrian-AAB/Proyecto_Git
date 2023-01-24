412f62a78d0b36030bbdca347391ad02f01d65dc
Virginia Pino <105395300+virginiapinol@users.noreply.github.com>
2023-01-24 11:53:47 -0300

    Al realizar merge no debe generar pull request

--- Program.cs
+++ Program.cs
@@ -1,3 +1,3 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("prueba_!");
-// Prueba correcta, no debe generar otro pull request
+// Prueba correcta, Al realizar merge no debe generar pull request
